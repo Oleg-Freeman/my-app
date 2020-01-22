0643e8c6c255440e992ec1fa0b7bf0854b8aa35e
Oleg Freeman <av.ssf4@gmail.com>
2020-01-22 18:47:46 +0200

    Some error?

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,13 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header("Access-Control-Allow-Headers", "Origin,Content-Type, Authorization, x-id, Content-Length, X-Requested-With");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   // next();
+// });
 app.use(cors());
 app.use(express.json());
 
